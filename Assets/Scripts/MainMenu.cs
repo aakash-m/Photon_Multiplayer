0dad336333e35b69737f6ac1464071ef13730f09
Aakash Makhija <43120661+aakash-m@users.noreply.github.com>
2021-02-18 00:58:17 +0100

    Lobby Created
    Main menu scene to Game scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createRoomText;
+    public TMP_InputField joinRoomText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom(createRoomText.text, roomOptions);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinRoomText.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+}
